44a4f867a850802086240299bd0ab9424de49ac5
Pankajsingh555 <146530493+Pankajsingh555@users.noreply.github.com>
2024-01-06 14:46:21 +0530

    Create withoutargumentwithoutreturntype.c

---
+++ withoutargumentwithoutreturntype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    void add();
+    add();
+
+}void add(){
+
+    int x,y,z;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+
+    printf("Sum = %d\n",z);
+
+}
